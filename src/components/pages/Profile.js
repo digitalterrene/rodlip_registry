3e907c69799f7cefb67da3f790d3858db659973e
digitalterrene <110802979+digitalterrene@users.noreply.github.com>
2023-08-10 14:08:15 +0200

    corrected from useSate

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,4 +1,4 @@
-import React,{useSate,useEffect} from "react";
+import React,{useState,useEffect} from "react";
 import { AiOutlineLayout, AiOutlineShareAlt } from "react-icons/ai";
 import { BiLayout } from "react-icons/bi";
 import { HiOutlineLocationMarker } from "react-icons/hi";
