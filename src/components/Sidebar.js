09fdd3b401c0fea9b4e1236fe5fe6cb526dde5f3
larry <larrykingsten1@gmail.com>
2023-10-16 10:34:46 +0200

    redesigned the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,140 +1,22 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import {
   Card,
   Typography,
   List,
   ListItem,
-  ListItemPrefix,
-  ListItemSuffix,
-  Chip,
-  Button,
-  CardFooter,
-  CardBody,
-  Input,
   IconButton,
-  Dialog,
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-  Alert,
-  Avatar,
-  CardHeader,
   Drawer,
 } from "@material-tailwind/react";
-
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import logo from "../assets/components/logo.png";
-import {
-  IoFingerPrint,
-  IoFingerPrintOutline,
-  IoShieldCheckmarkOutline,
-} from "react-icons/io5";
-import { usePathname, useRouter } from "next/navigation";
-import {
-  HiOutlineChartBar,
-  HiOutlineTemplate,
-  HiOutlineUser,
-} from "react-icons/hi";
-import { RiSuitcaseLine } from "react-icons/ri";
-
-import { FaBabyCarriage, FaUserSlash } from "react-icons/fa";
-import { BsChevronDown, BsGlobeAmericas, BsPlusLg } from "react-icons/bs";
+import { IoFingerPrint } from "react-icons/io5";
+import { FaBabyCarriage } from "react-icons/fa";
+import { BsGlobeAmericas } from "react-icons/bs";
 import { useGlobalContext } from "@/context/GlobalContext";
-import { MdOutlineWhatshot } from "react-icons/md";
 import Image from "next/image";
-import regs from "../assets/components/registries.png";
-import { BiDownArrow } from "react-icons/bi";
 import Link from "next/link";
 export default function MT_Sidebar() {
-  const [open, setOpen] = React.useState(0);
-  const [selectedAccount, setSelectedAccount] = useState({});
-  const [accessKey, setAccessKey] = useState("");
-  const [participations, setParticipations] = React.useState([]);
-  const [openLeft, setOpenLeft] = React.useState(true);
   const { toggleSidebar, setToggleSidebar } = useGlobalContext();
-  const pathname = usePathname();
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
-  };
-
-  const openDrawerLeft = () => setOpenLeft(true);
-  const closeDrawerLeft = () => setOpenLeft(false);
-  const [openDialog, setOpenDialog] = React.useState(toggleSidebar);
-
-  const handleOpenDialog = () => setOpenDialog((cur) => !cur);
-
-  const router = useRouter();
-  const dashboard_routes = [
-    {
-      route: "Enterprises",
-      link: "/small_medium_enterprises",
-      icon: "https://cdn-icons-png.flaticon.com/128/6116/6116024.png",
-    },
-    {
-      route: "corporations",
-      link: "/corporations",
-      icon: "https://cdn-icons-png.flaticon.com/128/9372/9372181.png",
-    },
-    {
-      route: "sole Traders",
-      link: "/sole_traders",
-      icon: "https://cdn-icons-png.flaticon.com/128/5654/5654627.png",
-    },
-    {
-      route: "Non Profits",
-      link: "/non_profit_organizations",
-      icon: "https://cdn-icons-png.flaticon.com/128/2967/2967188.png",
-    },
-    {
-      route: "institutions",
-      link: "/institutions",
-      icon: "https://cdn-icons-png.flaticon.com/128/9490/9490815.png",
-    },
-    {
-      route: "Real Estates",
-      link: "/real_estates",
-      icon: "https://cdn-icons-png.flaticon.com/128/602/602290.png",
-    },
-    {
-      route: "users",
-      link: "/users",
-      icon: "https://cdn-icons-png.flaticon.com/128/5080/5080751.png",
-    },
-    {
-      route: "virtual",
-      link: "/virtual",
-      icon: "https://cdn-icons-png.flaticon.com/128/10781/10781839.png",
-    },
-  ];
-  const dashboard_routesc = [
-    {
-      route: "Entities",
-      link: "/entities/small_medium_enterprises",
-      icon: <RiSuitcaseLine />,
-    },
-    {
-      route: "Registries",
-      link: "/future_world_civilizations/registries",
-      icon: <IoFingerPrintOutline />,
-    },
-    {
-      route: "Trending",
-      link: "/trending/small_medium_enterprises",
-      icon: <MdOutlineWhatshot />,
-    },
-    {
-      route: "Profile",
-      link: "/profile",
-      icon: <HiOutlineUser />,
-    },
-    {
-      route: "signin",
-      link: "/signin",
-      icon: <IoShieldCheckmarkOutline />,
-    },
-  ];
 
   return (
     <React.Fragment className=" bg-red-900">
@@ -165,48 +47,6 @@ export default function MT_Sidebar() {
         </div>
         <Card className="p-2 flex flex-col  z-40 overflow-x-clip  mx-auto justify-between    shadow-xl shadow-blue-gray-900/5">
           <div className=" ">
-            <List className="p-0">
-              <ListItem className="flex items-center">
-                {" "}
-                <IoFingerPrint className="h-5 w-5" />
-                <Typography
-                  color="blue-gray"
-                  className="mr-auto ml-3 font-semibold"
-                >
-                  Registries
-                </Typography>
-              </ListItem>
-              <div className="ml-3">
-                <ListItem>
-                  <Link
-                    onClick={() => setToggleSidebar(false)}
-                    href={
-                      "/registries/federal_ecopolitical_noble_assembly_committe"
-                    }
-                  >
-                    Fenac
-                  </Link>
-                </ListItem>
-                <ListItem>
-                  <Link
-                    onClick={() => setToggleSidebar(false)}
-                    href={"/registries/employment_consumption_equating_agency"}
-                  >
-                    Ecea
-                  </Link>
-                </ListItem>
-                <ListItem>
-                  <Link
-                    onClick={() => setToggleSidebar(false)}
-                    href={
-                      "/registries/morality_enforcement_and_retaintion_body"
-                    }
-                  >
-                    Mearb
-                  </Link>
-                </ListItem>
-              </div>
-            </List>
             <List className="p-0">
               <ListItem className="flex items-center">
                 {" "}
--- src/components/native/Sidebarr.js
+++ src/components/native/Sidebarr.js
@@ -14,11 +14,11 @@ export default function Sidebarr({ tabs, logo, avatar }) {
   const router = useRouter();
   return (
     <>
-      <div className="sticky  py- 2 bg-[#f5f3ff] shadow-md  bottom-0  top-0 h-screen">
+      <div className="sticky text-white py- 2 bg-[#151221] shadow-md  bottom-0  top-0 h-screen">
         <div
           className={`${
             !mini_heading ? "w-56" : "w-20"
-          } flex flex-col  overflow-y-auto h-full justify-between   p-4 pt-2  min-h-screen rounded-2xl`}
+          } flex flex-col  overflow-y-auto h-full justify-between   py-4 pt-2  min-h-screen rounded-2xl`}
         >
           <div className="w-full">
             <div className="ml-auto  flex items-center w-fit">
@@ -55,49 +55,9 @@ export default function Sidebarr({ tabs, logo, avatar }) {
                 </Typography>
               )}
             </div>
-            <div className="flex gap-2 mt-8 flex-col">
+            <div className="flex gap-2  text-white flex-col">
               {!mini_heading && (
-                <div className="flex items-center">
-                  <IoFingerPrintSharp className="text-xl mr-2" />
-                  <Typography className="text-gray-500">Registries</Typography>
-                </div>
-              )}
-
-              {tabs &&
-                tabs.slice(0, 5).map(({ icon, route, link }, i) => (
-                  <Button
-                    onClick={() => {
-                      setActiveTab(route);
-                      router.push(`/registries/${link}`);
-                    }}
-                    className={`flex w-full  shadow-none p-2 hover:bg-white py-1 items-center bg-${
-                      active_tab === route ? "black" : "transparent"
-                    } border-${active_tab === route && "2"}`}
-                  >
-                    <Tooltip content={`${route}`} placement="right">
-                      <img
-                        className={`w-${
-                          mini_heading ? "20" : "10"
-                        } p-1 rounded-lg bg-${
-                          active_tab === route ? "white" : ""
-                        }`}
-                        src={icon}
-                      />
-                    </Tooltip>
-
-                    {!mini_heading && (
-                      <Typography
-                        className={`capitalize   transition-all text-start w-32 ml-3 p-1 rounded-lg text-${
-                          active_tab === route ? "white" : "black"
-                        }`}
-                      >
-                        {route}
-                      </Typography>
-                    )}
-                  </Button>
-                ))}
-              {!mini_heading && (
-                <div className="flex  mt-6 border-t pt-6 items-center">
+                <div className="flex  mt-6 border-t border-gray-800 pt-6 items-center">
                   <HiOutlineLocationMarker className="text-xl mr-2" />
                   <Typography className="text-gray-500">Continents</Typography>
                 </div>
@@ -110,7 +70,7 @@ export default function Sidebarr({ tabs, logo, avatar }) {
                       setActiveTab(route);
                       router.push(`/continents/${link}`);
                     }}
-                    className={`flex w-full  shadow-none p-2 hover:bg-white py-1 items-center bg-${
+                    className={`flex w-full text-white rounded-none hover:shadow-none drop-shadow-none shadow-none p-2 hover:bg-white py-1 items-center bg-${
                       active_tab === route ? "black" : "transparent"
                     } border-${active_tab === route && "2"}`}
                   >
@@ -125,9 +85,7 @@ export default function Sidebarr({ tabs, logo, avatar }) {
 
                     {!mini_heading && (
                       <Typography
-                        className={`capitalize   transition-all text-start w-32 ml-3 p-1 rounded-lg text-${
-                          active_tab === route ? "white" : "black"
-                        }`}
+                        className={`capitalize text-white shodow-none drop-shadow-none  transition-all text-start w-32 ml-3 p-1 rounded-lg  hover:text-black`}
                       >
                         {route}
                       </Typography>
@@ -145,9 +103,9 @@ export default function Sidebarr({ tabs, logo, avatar }) {
                   <Button
                     onClick={() => {
                       setActiveTab(route);
-                      router.push(`/generations/${link}`);
+                      router.push(`/continents/${link}`);
                     }}
-                    className={`flex w-full  shadow-none p-2 hover:bg-white py-1 items-center bg-${
+                    className={`flex w-full text-white rounded-none hover:shadow-none drop-shadow-none shadow-none p-2 hover:bg-white py-1 items-center bg-${
                       active_tab === route ? "black" : "transparent"
                     } border-${active_tab === route && "2"}`}
                   >
@@ -162,9 +120,7 @@ export default function Sidebarr({ tabs, logo, avatar }) {
 
                     {!mini_heading && (
                       <Typography
-                        className={`capitalize   transition-all text-start w-32 ml-3 p-1 rounded-lg text-${
-                          active_tab === route ? "white" : "black"
-                        }`}
+                        className={`capitalize text-white shodow-none drop-shadow-none  transition-all text-start w-32 ml-3 p-1 rounded-lg  hover:text-black`}
                       >
                         {route}
                       </Typography>
